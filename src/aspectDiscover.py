b2283d4f11dda5493de352ebec1dbedeaa902f5b
yanliu8 <1234qwer737@sina.com>
2016-05-18 17:11:51 +0800

    aspect discovering

--- src/aspectDiscover.py
+++ src/aspectDiscover.py
@@ -1,5 +1,6 @@
 from textblob import TextBlob
 from textblob import Word
+from sklearn.cluster import KMeans
 class AspectDiscover(object):
 
 	def __init__(self, k):
@@ -26,21 +27,33 @@ class AspectDiscover(object):
 					if word not in self.stopwords:
 						w = Word(word)
 						tmp.append(w.lemmatize())
-						if word not in self._wordfrequency:
+						if word not in self.wordfrequency:
 							self.wordfrequency[word] = review.word_counts[word]
-				self._sentences.append(tmp)
+				self.sentences.append(tmp)
 
 	def buildVectors(self):
 		temp = sorted(self.wordfrequency, key=self.wordfrequency.__getitem__, reverse = True)
 		index = 0
 		while index < len(temp) and index < 100:
 			self.keywords.append(temp[index])
+			index += 1
 		for sentence in self.sentences:
 			vec = []
 			for keyword in self.keywords:
 				vec.append(sentence.count(keyword))
 			self.vectors.append(vec)
-		
+
+	def discover(self):
+		cluster = KMeans(n_clusters = self.k, n_jobs = -1)
+		cluster.fit(self.vectors)
+		for center in cluster.cluster_centers_:
+			aspectkeywords = sorted(range(len(center)), key=lambda x: center[x], reverse = True)
+			if len(aspectkeywords) > 10:
+				aspectkeywords = aspectkeywords[:10]
+			aspectkeywords = [self.keywords[x] for x in aspectkeywords]
+			print(aspectkeywords)
+
+
 
 
 
